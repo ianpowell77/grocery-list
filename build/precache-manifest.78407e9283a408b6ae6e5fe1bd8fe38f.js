25539155690ffb8912901cb6ec50c4d7d50f6233
Ian Powell <ianpowell77@yahoo.com>
2020-07-23 12:00:37 -0400

    removing build from .gitignore

---
+++ build/precache-manifest.78407e9283a408b6ae6e5fe1bd8fe38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e70ba9b6c9a8792716224288e388070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4df0226cd384b2bbd3d6",
+    "url": "/static/css/main.b0e1eaf0.chunk.css"
+  },
+  {
+    "revision": "bd7c0a817be5cb7d2335",
+    "url": "/static/js/2.35285ece.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35285ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df0226cd384b2bbd3d6",
+    "url": "/static/js/main.100abb93.chunk.js"
+  },
+  {
+    "revision": "7bcf3929e8d0347cbb29",
+    "url": "/static/js/runtime-main.7cb97436.js"
+  }
+]);
